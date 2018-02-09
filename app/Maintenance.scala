a4914cc1509dd479f742f7e41ce6ee2cc375e8cf
kirankbs <kiranprofessional2@gmail.com>
2018-02-10 00:42:23 +0530

    Add CORS

--- app/Maintenance.scala
+++ app/Maintenance.scala
@@ -1,6 +1,78 @@
 import dao.DBConfigFactory._
 import scala.collection.JavaConverters._
 
+object removeAllAttendance extends App{
+
+  val documents = documentClient.queryDocuments(
+    "dbs/" + databaseId + "/colls/" + collectionId,
+    "SELECT * FROM tyrion where tyrion.doctype=\"attendance\" ",
+    null).getQueryIterable().asScala.toList
+
+  println(documents)
+
+  documents.foreach(d => {
+    documentClient.deleteDocument(d.getSelfLink(), null)
+  })
+
+}
+object removeAllThr extends App{
+
+  val documents = documentClient.queryDocuments(
+    "dbs/" + databaseId + "/colls/" + collectionId,
+    "SELECT * FROM tyrion where tyrion.doctype=\"thr\" ",
+    null).getQueryIterable().asScala.toList
+
+  println(documents)
+
+  documents.foreach(d => {
+    documentClient.deleteDocument(d.getSelfLink(), null)
+  })
+
+}
+object removeAllHotCooked extends App{
+
+  val documents = documentClient.queryDocuments(
+    "dbs/" + databaseId + "/colls/" + collectionId,
+    "SELECT * FROM tyrion where tyrion.doctype=\"hot-cooked\" ",
+    null).getQueryIterable().asScala.toList
+
+  println(documents)
+
+  documents.foreach(d => {
+    documentClient.deleteDocument(d.getSelfLink(), null)
+  })
+
+}
+object removeAllRegistration extends App{
+
+  val documents = documentClient.queryDocuments(
+    "dbs/" + databaseId + "/colls/" + collectionId,
+    "SELECT * FROM tyrion where  tyrion.doctype=\"registration\" ",
+    null).getQueryIterable().asScala.toList
+
+  println(documents)
+
+  documents.foreach(d => {
+    documentClient.deleteDocument(d.getSelfLink(), null)
+  })
+
+}
+object removeAllImage extends App{
+
+  val documents = documentClient.queryDocuments(
+    "dbs/" + databaseId + "/colls/" + collectionId,
+    "SELECT * FROM tyrion where  tyrion.doctype=\"image\" ",
+    null).getQueryIterable().asScala.toList
+
+  println(documents)
+
+  documents.foreach(d => {
+    documentClient.deleteDocument(d.getSelfLink(), null)
+  })
+
+}
+
+
 object getAllRegistrationData extends App{
   val dashboard = documentClient.queryDocuments(
     "dbs/" + databaseId + "/colls/" + collectionId,
@@ -16,80 +88,4 @@ object getImageData extends App{
     null).getQueryIterable().toList()
 
   println("master data ===>" + dashboard)
-}
-object removeAllAttendance extends App{
-
-  val documents = documentClient.queryDocuments(
-    "dbs/" + databaseId + "/colls/" + collectionId,
-    "SELECT * FROM tyrion where tyrion.doctype=\"attendance\" ",
-    null).getQueryIterable().asScala.toList
-
-  println(documents)
-
-  documents.foreach(d => {
-    documentClient.deleteDocument(d.getSelfLink(), null)
-  })
-
-}
-
-object removeAllThr extends App{
-
-  val documents = documentClient.queryDocuments(
-    "dbs/" + databaseId + "/colls/" + collectionId,
-    "SELECT * FROM tyrion where tyrion.doctype=\"thr\" ",
-    null).getQueryIterable().asScala.toList
-
-  println(documents)
-
-  documents.foreach(d => {
-    documentClient.deleteDocument(d.getSelfLink(), null)
-  })
-
-}
-
-
-object removeAllHotCooked extends App{
-
-  val documents = documentClient.queryDocuments(
-    "dbs/" + databaseId + "/colls/" + collectionId,
-    "SELECT * FROM tyrion where tyrion.doctype=\"hot-cooked\" ",
-    null).getQueryIterable().asScala.toList
-
-  println(documents)
-
-  documents.foreach(d => {
-    documentClient.deleteDocument(d.getSelfLink(), null)
-  })
-
-}
-
-
-object removeAllRegistration extends App{
-
-  val documents = documentClient.queryDocuments(
-    "dbs/" + databaseId + "/colls/" + collectionId,
-    "SELECT * FROM tyrion where  tyrion.doctype=\"registration\" ",
-    null).getQueryIterable().asScala.toList
-
-  println(documents)
-
-  documents.foreach(d => {
-    documentClient.deleteDocument(d.getSelfLink(), null)
-  })
-
-}
-
-object removeAllImage extends App{
-
-  val documents = documentClient.queryDocuments(
-    "dbs/" + databaseId + "/colls/" + collectionId,
-    "SELECT * FROM tyrion where  tyrion.doctype=\"image\" ",
-    null).getQueryIterable().asScala.toList
-
-  println(documents)
-
-  documents.foreach(d => {
-    documentClient.deleteDocument(d.getSelfLink(), null)
-  })
-
 }
